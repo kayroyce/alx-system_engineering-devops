18259f853bdbfa7a618370e7206d90992f3345df
kayroyce <kayroyce2005@gmail.com>
2022-10-14 14:36:04 -0700

    task 1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+file {'school':
+  ensure     => file,
+  path       => '/tmp/school',
+  permission => '0744',
+  owner      => 'www-data',
+  group      => 'www=data',
+  contains   => 'I love Puppet',
+}
+export RUBYOPT='-W0'
