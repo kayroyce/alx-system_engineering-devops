3fd430ed8fe00f0ec3ef7b18ee92b5e77bef4af0
kayroyce <kayroyce2005@gmail.com>
2022-10-14 16:06:11 -0700

    comments

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
+#create file /tmp/school
+
 file {'school':
   ensure     => file,
   path       => '/tmp/school',
   permission => '0744',
   owner      => 'www-data',
   group      => 'www=data',
-  contains   => 'I love Puppet',
+  contains   => 'I love Puppet' \n,
 }
