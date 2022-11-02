315ddb8843fc0acc0f04338184649ae7ddcf9685
Olawale Kolawole <102330728+kayroyce@users.noreply.github.com>
2022-11-02 09:54:04 +0100

    update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,36 +1,20 @@
-# Puppet manifest to install nginx
+# Install and config the nginx
 package { 'nginx':
   ensure => installed,
-}
-
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  name   => 'nginx',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World!',
+  content => 'Hello World',
+  path    => '/var/www/html/index.html'
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}# Puppet manifest to install nginx
-package { 'nginx':
-  ensure => installed,
-}
-
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+file_line { 'title':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true
 }
 
 service { 'nginx':
