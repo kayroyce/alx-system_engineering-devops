93a24f7735ba7edd50956b812039bc0477f41cd8
Olawale Kolawole <102330728+kayroyce@users.noreply.github.com>
2022-11-29 04:32:21 +0100

    lint error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 #create file /tmp/school
 
-file {'school':
-  path       => '/tmp/school',
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www=data',
-  content   => 'I love Puppet',
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www=data',
+  content => 'I love Puppet',
 }
