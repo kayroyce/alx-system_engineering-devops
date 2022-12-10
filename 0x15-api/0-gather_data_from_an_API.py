84aa440faf2f065f0a2fc831d88ff1f6c83b98e2
kayroyce <kayroyce2005@gmail.com>
2022-12-10 15:50:59 +0100

    redo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,8 +6,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    id_em = argv[1]
-    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_em)
+    id = argv[1]
+    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id)
     url_todos = url_employ + "/todos"
     r_employ = requests.get(url_employ).json()
     r_todos = requests.get(url_todos).json()
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     output = "Employee {} is done with tasks({}/{}):".format(
                 name, len(done_task), len(total_num_task))
     for task in done_task:
-        output += "\n\t " + task.get("title")
+        output += "\n\t" + task.get("title")
     print(output)
